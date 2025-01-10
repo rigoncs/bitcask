b0cb966d4a9938245677aef8559d31da1a5c4817
Rigon Liu <2412789408@qq.com>
2025-01-10 11:28:38 +0800

    String结构支持

---
+++ redis/types.go
@@ -0,0 +1,85 @@
+package redis
+
+import (
+	"bitcask"
+	"encoding/binary"
+	"errors"
+	"time"
+)
+
+var (
+	ErrWrongTypeOperation = errors.New("WRONGTYPE Operation against a key holding the wrong kind of value")
+)
+
+// RedisDataStructure Redis 数据结构服务
+type RedisDataStructure struct {
+	db *bitcask.DB
+}
+
+type redisDataType = byte
+
+const (
+	String redisDataType = iota
+	Hash
+	Set
+	List
+	ZSet
+)
+
+// NewRedisDataStructure 新建 Redis 数据结构服务
+func NewRedisDataStructure(options bitcask.Options) (*RedisDataStructure, error) {
+	db, err := bitcask.Open(options)
+	if err != nil {
+		return nil, err
+	}
+	return &RedisDataStructure{db: db}, nil
+}
+
+// ============== String 数据结构 ============
+
+// Set 设置一个 String 的值
+func (rds *RedisDataStructure) Set(key []byte, ttl time.Duration, value []byte) error {
+	if value == nil {
+		return nil
+	}
+
+	// 编码 value: type + expire + payload
+	buf := make([]byte, 1+binary.MaxVarintLen64)
+	buf[0] = String
+	var index = 1
+	var expire int64 = 0
+	if ttl != 0 {
+		expire = time.Now().Add(ttl).UnixNano()
+	}
+	index += binary.PutVarint(buf[index:], expire)
+
+	encValue := make([]byte, index+len(value))
+	copy(encValue[:index], buf[:index])
+	copy(encValue[index:], value)
+
+	// 调用存储接口写入数据
+	return rds.db.Put(key, encValue)
+}
+
+func (rds *RedisDataStructure) Get(key []byte) ([]byte, error) {
+	encValue, err := rds.db.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// 解码 value
+	dataType := encValue[0]
+	if dataType != String {
+		return nil, ErrWrongTypeOperation
+	}
+
+	var index = 1
+	expire, n := binary.Varint(encValue[index:])
+	index += n
+	// 判断是否过期
+	if expire > 0 && expire <= time.Now().UnixNano() {
+		return nil, nil
+	}
+
+	return encValue[index:], nil
+}
